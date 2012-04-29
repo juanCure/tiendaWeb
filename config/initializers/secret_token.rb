82107b226a4df92be879951a2dbce88358084d99
Juan Manuel Rodriguez <manuel2489@gmail.com>
2012-04-28 23:03:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TiendaWeb::Application.config.secret_token = 'ebc17720b6b94e27f1583dfd32daed3c7a8a96e15f32c39b3e1fc93daa3422612c73d48c3b4dc812226ee5f8cac6094043db7e0a6717b14c1306d79857eb9967'
